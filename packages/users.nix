17cd508aea203cf7fb8a25b4f821d147c8e157ac
Justin Speegle <justin@speegs.com>
2022-09-11 19:25:03 -0700

    update users with functions

--- packages/users.nix
+++ packages/users.nix
@@ -1,34 +1,33 @@
-{ config, pkgs, ... }:
+{ lib, config, pkgs, ... }:
 
 let
   hm_uri = "https://github.com/nix-community/home-manager/archive/master.tar.gz";
   hm = builtins.fetchTarball hm_uri;
+  base_user = { pw, desc}: {
+    isNormalUser = true;
+    extraGroups = [ "networkmanager" ];
+    password = pw;
+    description = desc;
+  };
 
 in {
   imports = [(import "${hm}/nixos")];
 
-  users.users.test = {
-    isNormalUser = true;
-    extraGroups = [ "netorkmanager" ];
-  };
-  home-manager.users.test = {
-    home.stateVersion = "22.05";
-    programs.tmux = {
-      extraConfig = builtins.readFile ./tmux_airline;
-    };
-  };
-  
-  users.users.ugflows = {
-    isNormalUser = true;
-    description = "Justin Speegle";
-    extraGroups = [ "networkmanager" "wheel" ];
+  users.users = {
+    guest = ( base_user { pw = "guest"; desc= "Guest User"; });
+
+    ugflows = lib.mkMerge[
+      ( base_user { pw = "ugflows"; desc = "Justin Speegle"; })
+      { extraGroups = [ "wheel" ]; }
+    ];
   };
 
   home-manager.users.ugflows = {
     home.stateVersion = "22.05";
+
     programs.tmux = {
-      extraConfig = builtins.readFile ./tmux.conf
-        + builtins.readFile ./tmux_airline;
+      enable = true;
+      extraConfig = builtins.readFile ./tmux.conf;
     };
 
     programs.git = {
