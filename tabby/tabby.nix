3594e35323aa5f09bd3c2c8e770c393e6d2caf40
Justin Speegle <justin@speegs.com>
2023-03-04 15:01:13 -0800

    tabby libinput

--- tabby/tabby.nix
+++ tabby/tabby.nix
@@ -1,4 +1,14 @@
 { ... }: {
   networking.hostName = "tabby";
   imports = [ ./hardware-configuration.nix ];
+  
+  services.xserver.libinput = {
+    enable = true;
+
+    touchpad = {
+      naturalScrolling = true;
+      disableWhileTyping = true;
+      tappingButtonMap = "lrm";
+    };
+  };
 }
