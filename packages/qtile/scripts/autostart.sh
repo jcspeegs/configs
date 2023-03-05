30b342c247da78f7149f440bf24a72d71794359f
Justin Speegle <justin@speegs.com>
2023-03-05 14:25:08 -0800

    fix overlapping screens

--- packages/qtile/scripts/autostart.sh
+++ packages/qtile/scripts/autostart.sh
@@ -16,7 +16,7 @@ function run {
 #Find out your monitor name with xrandr or arandr (save and you get this line)
 case $HOSTNAME in
     lightshow)
-        xrandr --output DP-2 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-0 --mode 1920x1080 --scale 1.1 --pos 4480x0 --rotate normal --output DP-1 --off --output HDMI-0 --mode 1920x1080 --scale 1.1 --pos 2560x0 --rotate normal --output DP-2 --off --output DP-4 --off --output DP-5 --off --output USB-C-0 --off
+        xrandr --output DP-0 --mode 1920x1080 --pos 4672x0 --rotate normal --scale 1.1 --output DP-1 --off --output HDMI-0 --mode 1920x1080 --pos 2560x0 --rotate normal --scale 1.1 --output DP-2 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-3 --off --output DP-4 --off --output DP-5 --off --output USB-C-0 --off
         ;;
     tabby)
         xrandr --output eDP-1 --primary --mode 2736x1824 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --dpi 200
