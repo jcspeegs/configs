b7c9809050e52360d57b4305845cc3d0abdbdb60
Justin Speegle <justin@speegs.com>
2023-04-07 22:30:02 -0700

    update lightshow wifi adapator name, need to make this dynamic at some point ?!

--- lightshow/lightshow.nix
+++ lightshow/lightshow.nix
@@ -1,6 +1,6 @@
 { ... }: {
   networking.hostName = "lightshow";
   services.xserver.videoDrivers = [ "nvidia" ];
-  environment.sessionVariables = rec { wifi_adapter = "wlp0s20f0u4"; };
+  environment.sessionVariables = rec { wifi_adapter = "wlp0s20f0u3u2"; };
   imports = [ ./hardware-configuration.nix ];
 }
