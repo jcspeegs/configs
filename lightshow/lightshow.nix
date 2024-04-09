9027730eac5c0565c1673e6ee34d1e72ebc176a8
Justin Speegle <justin@speegs.com>
2024-04-09 12:30:42 -0700

    update nvidia config

--- lightshow/lightshow.nix
+++ lightshow/lightshow.nix
@@ -1,6 +1,15 @@
 { ... }: {
   networking.hostName = "lightshow";
+
   services.xserver.videoDrivers = [ "nvidia" ];
+  # https://nixos.wiki/wiki/Nvidia
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    open = false;
+    nvidiaSettings = true;
+  };
+
   environment.sessionVariables = rec { wifi_adapter = "wlp0s20f0u3u2"; };
   imports = [ ./hardware-configuration.nix ];
 }
