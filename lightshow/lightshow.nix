6745a19f58f75890d54ecdb563be370d99e1890d
ugflows <justin@speegs.com>
2024-12-10 13:47:47 -0800

    fixup

--- lightshow/lightshow.nix
+++ lightshow/lightshow.nix
@@ -15,10 +15,11 @@
   environment.sessionVariables = rec { wifi_adapter = "wlp0s20f0u3u2"; };
   imports = [ ./hardware-configuration.nix ];
 
-  fileSystems."/mnt/data" = {
+  fileSystems."/data" = {
     device = "/dev/desk/by-uuid/d935e28a-867f-49ae-acd0-1f26b27a163c";
     fsType = "ext4";
     options = [
+      "defaults"
       "nofail"
     ];
   };
